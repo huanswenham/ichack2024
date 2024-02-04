5bea2f7fb79934af95020fe11aaf1c6d9c9908d2
hsh121 <huan.ham21@imperial.ac.uk>
2024-02-04 01:42:02 +0000

    Change debug logging append method

--- Assets/Scripts/DebugManager.cs
+++ Assets/Scripts/DebugManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class DebugManager : MonoBehaviour
 {
     public void PrintDebug (string log) {
-        GetComponent<TextMeshProUGUI>().text += "\n";
+        GetComponent<TextMeshProUGUI>().text += ", ";
         GetComponent<TextMeshProUGUI>().text += log;
     } 
 }
